19a95740ef9b811f9fba0f554774ef3237b83c41
Yuvanesh-ux <68208096+Yuvanesh-ux@users.noreply.github.com>
2023-02-28 17:35:05 -0500

    codespace_setup

--- yuvanesh-io/src/components/Experience.js
+++ yuvanesh-io/src/components/Experience.js
@@ -1,7 +1,36 @@
+import { Navbar } from "./Navbar";
+import { FaPython, FaReact, FaHtml5 } from "react-icons/fa"
+import { SiPytorchlightning } from "react-icons/si"
+
 export function Experience() {
   return (
     <div>
-      <h1>Experience</h1>
+      <body>
+        <main className="experience-main" id='main'>
+          <div className="experience-card-container">
+            <div className="experience-top-card">
+            </div>
+            <div className="experience-bottom-card">
+              <div className="bottom-card-textcontainer">
+                <p className="para-1">- Leveraging React.Js and Python to create beautiful and intuitive FE experiences for Nomic's Atlas maps</p>
+                <p className="para-2">- Used algorithms such as k-means and agglomerate clustering to generate automated topics from mappings of data</p>
+              </div>
+              <h1 className="junior-topright">Junior Software Engineer</h1>
+            </div>
+          </div>
+          <div className="logo-container">
+            <div className="logo-container-left">
+              <div className="logo-react"><FaReact className="react hvr-grow-rotate"/></div>
+              <div className="logo-python"><FaPython className="python "/></div>
+            </div>
+            <div className="logo-container-right">
+              <div className="logo-pytorch"><SiPytorchlightning className="pytorch hvr-shrink"/></div>
+              <div className="logo-htmlcss"><FaHtml5 className="hvr-bob"/></div>
+            </div>
+          </div>
+        </main>
+        <Navbar/>
+      </body>
     </div>
   );
 }
--- yuvanesh-io/src/components/Home.js
+++ yuvanesh-io/src/components/Home.js
@@ -1,32 +1,8 @@
 import '../App.css';
-import {GiHamburgerMenu} from 'react-icons/gi'
-import home from './imgs/home.png'
-import experience from './imgs/experience.png'
 import React from 'react';
-import { useState } from 'react';
-import {
-  Link
-} from 'react-router-dom';
+import {Navbar} from './Navbar'
 
 export function Home() {
-    const [isOpened, setIsOpened] = useState(false)
-
-  const toggleNav = () => {
-    console.log(document.body.dataset.nav)
-    document.body.dataset.nav = document.body.dataset.nav == "true" ? "false" : "true";
-    if (isOpened === false) {
-      document.getElementById("main").style.transform = "translateY(-50%)"
-      document.getElementById("nav-links").style.transform = "translateY(0%) scale(1)"
-      document.getElementById("nav-toggle").style.backgroundColor = "#3c4cc7"
-      setIsOpened(true)
-    }
-    if (isOpened === true) {
-      document.getElementById("main").style.transform = "translateY(0)"
-      document.getElementById("nav-links").style.transform = "translateY(70%) scale(0.8)"
-      document.getElementById("nav-toggle").style.backgroundColor = "#B5BD68"
-      setIsOpened(false)
-    }
-  };
   
   return (
     <div>
@@ -48,28 +24,13 @@ export function Home() {
                 <h1 className='anand'> Anand </h1>
               </div>
               <div className='bottom-right-card'>
-                  <p className='coder'>Coder</p>
-                  <p className='artist'>Artist</p>
+                <p className='coder'>Coder</p>
+                <p className='artist'>Artist</p>
               </div>
             </div>
           </div>
         </main>
-        <nav>
-          <div id="nav-links">
-            <a className='nav-link' href="#">
-              <h2 className='nav-link-label'>Home</h2>
-              <img className='nav-link-image' src={home}/>
-            </a>
-            <a className='nav-link' href="/experience">
-              <h2 className='nav-link-label'>Experience</h2>
-              <img className='nav-link-image' src={experience}/>
-            </a>
-          </div>
-        </nav>
-
-        <button id="nav-toggle" type="button" onClick={toggleNav}>
-          <GiHamburgerMenu/>
-        </button>
+        <Navbar/>
       </body>
     </div>
   );
